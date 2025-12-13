b0d87cf27524e314f88a9c7f81032d7c1bafe108
khushi <joshikhushi0408@gmail.com>
2025-12-13 18:54:58 +0530

    Delete learnagain folder

--- learnAgain/fisrt.js
+++
@@ -1,3 +0,0 @@
-console.log("hello working in node ")
-console.error("this is error")
-console.log("i started learning javascript")
--- learnAgain/script.js
+++
@@ -1,5 +0,0 @@
-function muFunctio (){
-    // console.log("button clicked")
-    document.getElementById("first").innerHTML ='text changed';
-    document.getElementsByTagName('h1').innerHTML="khushi is best";
-   }
